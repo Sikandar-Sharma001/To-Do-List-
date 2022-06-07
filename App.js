40685980caa407db18688cda8952b71d9a9c3e48
Sikandar Sharma <sikandar.sharma.fiem.cse19@teamfuture.in>
2022-06-07 22:32:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./index.css"
+import Todo from './components/todoreact/todo';
+const App=()=>{
+  return(
+    <>
+    <Todo/>
+    </>
+  )
+}
+export default App;
